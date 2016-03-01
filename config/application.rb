84e3358935bf4730f3c86f8b80a1f55e5c14403c
Farruh <ufarruh@gmail.com>
2016-03-01 01:32:29 +0000

    Added custom font for navbar

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,9 @@ module Nomster
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
+    config.assets.precompile += %w(.svg .eot .woff .tff)
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
