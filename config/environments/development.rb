e2cf24d2f7c15198b748725a0e3324bece735dd4
Farruh <ufarruh@gmail.com>
2016-03-10 04:00:59 +0000

    Add auto email sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,17 @@
 Nomster::Application.configure do
-  config.action_mailer.default_url_options = {host: 'localhost:3030'}
-  # Settings specified here will take precedence over those in config/application.rb.
+  
+  config.action_mailer.smtp_settings ={
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.default_url_options = {host: 'localhost:3030'}
+  
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -15,7 +22,7 @@ Nomster::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
