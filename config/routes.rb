7aa13e3cd50c94dfa3721f3a0ec17c8a7ca97f19
Farruh <ufarruh@gmail.com>
2016-03-12 01:10:56 +0000

    Count user comments and 5 star reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Nomster::Application.routes.draw do
-  devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-root 'places#index'
+    devise_for :users
+    root 'places#index'
 
-resources :places do
-  resources :comments, only: :create
-  resources :photos, only: :create
-end
+    resources :places do
+      resources :comments, only: :create
+      resources :photos, only: :create
+    end
 
-resources :users, only: :show
+    resources :users, only: :show
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
